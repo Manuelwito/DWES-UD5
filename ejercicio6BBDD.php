c69882ce9085d49833a83d8a436759d444f14405
manuelwito <manuel.ramos.illana.al@iespoligonosur.org>
2021-11-26 14:30:28 +0100

    25nov

--- ejercicio6BBDD.php
+++ ejercicio6BBDD.php
@@ -1,35 +1,21 @@
+<?php
+$servidor = "localhost";
+$baseDatos = "autentificacion";
+$usuario = "developer";
+$pass = "developer";
 
-    <?php
-
-
-
-function getUser($nombre){
-
-    $servidor = "localhost";
-    $baseDatos = "autenticacion";
-    $usuario = "root";
-    $pass = "root";
-
-    try{
-        $conexion = new PDO("mysql:host=$servidor;dbname=$baseDatos", $usuario, $pass);
-        echo"Conectado correctamente";
-        echo"<br>";
-
-
-        $sql=$conexion->prepare("SELECT * FROM usuarios WHERE nombre = nombre");
-        $sql->bindParam("nombre", $nombre);
-        $sql->execute();
-
-        $datos = $sql->fetch(PDO::FETCH_ASSOC);
-        
-        return $datos;
-      
-        } catch(PDOException $e) {
-            echo"Conexion fallida: = " . $e->getMessage();
-        }
-        $conn=null;
-
-
+function getUser($nombreUsuario) {
+    try {
+        $conn = new PDO("mysql:host=$GLOBALS[servidor];dbname=$GLOBALS[baseDatos]", $GLOBALS["usuario"], $GLOBALS["pass"]);
+        $sql = $conn -> prepare("SELECT * FROM conexion WHERE NombreUsuario=?");
+        $sql -> bindParam(1, $nombreUsuario);
+        $sql -> execute();
+        $retorno = $sql->fetch(PDO::FETCH_ASSOC);
+        $conn = null;
+        return $retorno;
+    } catch(PDOException $e) {
+        return -1;
+    }
+    
 }
-
-    ?>
+?>
--- ejercicio7.php
+++ ejercicio7.php
@@ -1,61 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
+    <?php
     include 'ejercicio6BBDD.php';
 
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $nombre = $_POST['nombreUsuario'];
         $contrasenia = $_POST['contrasenia'];
 
         $datos = getUser($nombre);
 
-        if($datos){
+        if ($datos) {
             $contraseniaEncr = $datos["Contrasenia"];
-            $retorno = password_verify($contrasenia,$contraseniaEncr);
-            if($retorno){
+            $retorno = password_verify($contrasenia, $contraseniaEncr);
+            if ($retorno) {
 
-                echo "Contraseña correcta";   
+                echo "Contraseña correcta";
                 session_start();
-                $_SESSION['rol']=$datos['Rol'];
-                
-
-            }
-            else{
+                $_SESSION['rol'] = $datos['Rol'];
+            } else {
                 echo "Contraseña incorrecta";
             }
-        }
-        else{
+        } else {
             echo "Usuario o contraseña incorrecto";
-
         }
 
-        if($_SESSION['rol']=='admin'){
-                header('Location: admin.php');
-
-                    
-        }else if($_SESSION['rol']=='usuario'){
+        if ($_SESSION['rol'] == 'admin') {
+            header('Location: admin.php');
+        } else if ($_SESSION['rol'] == 'usuario') {
             header('Location: usuario.php');
-
-        }else{
+        } else {
             header('Location: ejercicio7.php');
         }
     }
-    
-    
-    
+
+
+
     ?>
-<form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="POST">
+    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
         <input type="text" name="nombreUsuario" placeholder="Nombre Usuario" required>
         <input type="password" name="contrasenia" placeholder="Contraseña" required>
         <input type="submit" value="Registrar">
-</form>
+    </form>
 </body>
+
 </html>
